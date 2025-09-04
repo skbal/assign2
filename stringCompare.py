0872dc586e73423e7e39bc47104ee88ffa642422
skbal <166859502+skbal@users.noreply.github.com>
2025-09-04 15:59:24 -0400

    Create stringCompare.py

---
+++ stringCompare.py
@@ -0,0 +1,7 @@
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+
+if string1.lower() == string2.lower():
+    print("The strings are equal (case-insensitive).")
+else:
+    print("The strings are unequal (case-insensitive).")
