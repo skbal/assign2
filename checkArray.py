0562a3e1cb54c1017df49a68fe4353f9c0ece4d5
skbal <166859502+skbal@users.noreply.github.com>
2025-09-04 16:06:48 -0400

    Create checkArray.py

---
+++ checkArray.py
@@ -0,0 +1,7 @@
+names = ["Sangram, "Sanvi", "Alice", "Bob", "Charlie", "Diana"]
+your_name = input("Enter your name: ")
+
+if your_name in names:
+    print("Welcome!")
+else:
+    print("See you next time.")
